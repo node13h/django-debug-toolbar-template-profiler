bc97388f08a1ab3b65af5066ef2b10c3840e6184
Sergej Alikov <sergej.alikov@gmail.com>
2017-05-03 19:26:48 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from io import open
 
 setup(
     name='django-debug-toolbar-template-profiler',
-    version='1.0.1',
+    version='1.0.2',
     description='Displays template rendering time on the timeline',
     long_description=open('README.rst', encoding='utf-8').read(),
     author='Sergej Alikov',
