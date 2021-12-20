4ab09e4ddebb0a28575c55d7ea1d48b4804cdf73
Steven Mapes <steve@stevenmapes.com>
2021-12-20 19:58:16 +0000

    Updating the translation for Django 3,2+
    Partial fix for #19 where the import of the translation library is outdated for Django 3.2+
    
    if django.VERSION < (3, 2):
        from django.utils.translation import ugettext_lazy as _
    else:
        from django.utils.translation import gettext_lazy as _

--- template_profiler_panel/panels/template.py
+++ template_profiler_panel/panels/template.py
@@ -6,8 +6,11 @@ import wrapt
 from debug_toolbar.panels import Panel
 from debug_toolbar.panels.sql.utils import contrasting_color_generator
 from django.dispatch import Signal
-from django.utils.translation import ugettext_lazy as _
-
+if django.VERSION < (3, 2):
+    from django.utils.translation import ugettext_lazy as _
+else:
+    from django.utils.translation import gettext_lazy as _
+    
 template_rendered = Signal(providing_args=[
     'instance', 'start', 'end', 'level', 'processing_timeline',
 ])
